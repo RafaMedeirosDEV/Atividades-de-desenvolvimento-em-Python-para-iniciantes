6afcc31477d33f4a4ccec5e3791010a88fc6c331
RafaMedeirosDEV <rafaela.m.social@gmail.com>
2025-08-22 18:40:43 -0300

    Calculando
    dobro, triplo e raiz

---
+++ atividade_fase1/exe_05b.py
@@ -0,0 +1,8 @@
+# resultado do dobro, triplo e raiz quadrada de um número
+
+num = int(float(input('Informe o número: ')))
+dob = num * 2
+tri = num * 3 
+raiz = num ** (1/2)
+
+print(f'O número digitado foi : {num}\nO dobro desse nº é {dob}\nO triplo desse nº é {tri}\nA raiz quadrada desse nº é {raiz}')
