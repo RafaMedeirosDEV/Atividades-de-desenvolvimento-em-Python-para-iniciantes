28fddbac2331772567a94334d61d4437aee93812
RafaMedeirosDEV <rafaela.m.social@gmail.com>
2025-08-22 18:26:09 -0300

    Ordem de precedência
    descrição da ordem de precedência para operações aritméticas em Python.

---
+++ atividade_fase1/exe_4.py
@@ -0,0 +1,5 @@
+# Ordem de Precedência para operadores Matemáticos.
+# 1º - () parenteses
+# 2º - ** Potência
+# 3º - * multiplicação, / divisão,  // divisão inteira ou % resto da divisão _ entre eles o que vier primeiro
+# 4º - + soma , - subtração _ o que vier primeiro entre eles
