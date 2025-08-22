661a8e0c89e69e4cf96c269833811e4ff086104c
RafaMedeirosDEV <rafaela.m.social@gmail.com>
2025-08-22 18:30:57 -0300

    antecessor e sucessor

---
+++ atividade_fase1/exe_05a.py
@@ -0,0 +1,4 @@
+#programa que mostra antecessor e sucessor de um número
+
+num = int(input('Digite um número'))
+print(f'O número digitado foi {num}, seu antecessor é {num-1} e seu sucessor é {num+1}')
