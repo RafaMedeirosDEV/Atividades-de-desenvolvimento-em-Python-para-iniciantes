efd6b8594381882ef54e7a3da029a6b315e4db57
RafaMedeirosDEV <rafaela.m.social@gmail.com>
2025-08-26 12:34:39 -0300

    conversão para Dolar
    convertendo um valor para dólar.
    obs.: Dólar do dia 26/08/2025 valendo $5,43

---
+++ atividade_fase1/exe_05f.py
@@ -0,0 +1,7 @@
+#convertendo valores em Real para dolar
+
+valor = float(input('Qual valor você deseja converter de Real para Dolar, R$:'))
+
+dolar = valor/5.43
+
+print(f'Com o valor de R$ {valor}, você consegue adquirir $ {dolar:.2f}.')
