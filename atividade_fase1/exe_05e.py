1dc19c0ee33aeee2e4371fb05c392cf5be0fd0ed
RafaMedeirosDEV <rafaela.m.social@gmail.com>
2025-08-26 12:04:44 -0300

    Tabuada
    criando a tabuada de um número escolhido pelo usuário.

---
+++ atividade_fase1/exe_05e.py
@@ -0,0 +1,8 @@
+#mostrando a taboada de um número
+
+num = int(input('Digite o número inteiro:'))
+
+
+print(f'O número é {num}.')
+print(f'Taboada de {num}:')
+print(f'1X{num} = {num*1}\n2X{num} = {num*2}\n3X{num} = {num*3}\n4X{num} = {num*4}\n5X{num} = {num*5}\n6X{num} = {num*6}\n7X{num} = {num*7}\n8X{num} = {num*8}\n9X{num} = {num*9}\n10X{num} = {num*10}')
